10e61b87f535a67b11edfb340cbf39bb0fbc5a29
Anton Gushcha <ncrashed@gmail.com>
2017-06-21 18:15:48 +0300

    Add example

---
+++ examples/01/src/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Data.Text.Normalize
+import System.Environment
+import Data.Foldable (traverse_)
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [p] -> processDoc p
+    _ -> putStrLn "Expected single argument: path to file to normalize"
+
+processDoc :: FilePath -> IO ()
+processDoc p = do
+  chunks <- normalizeSimple 100 <$> T.readFile p
+  putStrLn "Normalized chunks:"
+  traverse_ (T.putStrLn . T.unwords) chunks
